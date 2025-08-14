de8c6f238e570921885473b52090c9c433529762
Jay Chinche <94739010+jaychinche@users.noreply.github.com>
2025-08-14 16:43:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js in Docker!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
